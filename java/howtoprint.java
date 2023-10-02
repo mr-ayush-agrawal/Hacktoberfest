85ee6b641caf5ea486c80cb3b7d213a4bd214948
Xankush <ankushdohare12@gmail.com>
2023-10-02 17:40:40 +0530

    add some basic java code

---
+++ java/howtoprint.java
@@ -0,0 +1,7 @@
+public class howtoprint{
+    public static void main(string[] args){
+        System.out.println("type here what you want to print");
+        //shortcut for system.out.println();
+        // type sout then hit tab button 
+    }
+}
---
+++ java/solidrectangle.java
@@ -0,0 +1,13 @@
+public class solidrectangle {
+    public static void main(String[] args){
+        int n = 4;
+        int m = 5;
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
